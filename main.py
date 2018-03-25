c1775ac0967c1ef2598d82968154b4a541b41320
zzw890827 <zzw890827@gmail.com>
2018-03-25 22:00:06 +0900

    CSV出力。

--- main.py
+++ main.py
@@ -13,19 +13,10 @@ __copyright__ = 'Fujitsu General Limited'
 import sys
 from PyQt5.QtWidgets import QApplication
 from view import DialogWelcome
-# from dialog_items_view import DialogItems
-# from view import DialogFinish
-from view import DialogMaterSlaveTest
-from controller import build_master_slave_test_system
-from controller import create_vrf_system_list
-from controller import write_csv_file
-from model import ExistTestFunction
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    # ui = DialogMaterSlaveTest()
     ui = DialogWelcome()
     ui.show()
     sys.exit(app.exec_())
-    # test = create_vrf_system_list([1, 3, 5])
-    # write_csv_file(test, 'test.csv')
